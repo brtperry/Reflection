ae769dc0f158e354207c63e0dd705087d133111b
brtperry <brett386@gmail.com>
2016-01-07 14:39:15 +0000

    SQL parameters using attributes and reflection.
    Pass SQL parameters to DBCommand using attributes and reflection.

---
+++ Reflection/DataAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLAParameter
+{
+    [AttributeUsage(AttributeTargets.All)]
+    public class DataAttribute : Attribute
+    {
+        private string fieldName;
+
+        private bool include;
+
+        public DataAttribute(string fld, bool inc)
+        {
+            fieldName = fld;
+
+            include = inc;
+        }
+
+        public virtual string FieldName
+        {
+            get { return fieldName; }
+        }
+
+        public virtual bool Include
+        {
+            get { return include; }
+        }
+    }
+}
---
+++ Reflection/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLAParameter;
+using System.Data.SqlClient;
+using System.Data;
+using System.ComponentModel;
+
+namespace Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = new MySQLData() {
+
+                Age = 55,
+
+                Name = "Axel Pressbutton",
+
+                Other = "Cyborg"
+            };
+
+            var controller = new SQLDataController(data);
+
+            var response = controller.Save();
+
+            if (response)
+            {
+                Console.WriteLine("Everyone's happy.");
+            }
+            else
+            {
+                Console.WriteLine("No one's happy.");
+            }
+        }
+    }
+
+    class MySQLData
+    {
+        [DataAttribute("@Name", true)]
+        public string Name { get; set; }
+
+        [DataAttribute("@Age", true)]
+        public int Age { get; set; }
+
+        [DataAttribute("@Other", false)]
+        public string Other { get; set; }
+    }
+
+    class SQLDataController
+    {
+        private MySQLData myData;
+
+        public SQLDataController(MySQLData data)
+        {
+            myData = data;
+        }
+
+        public bool Save()
+        {
+            const string cnn = @"Server=.\SQLEXPRESS;Initial Catalog=LiquidPOS;Trusted_Connection=True;Connect Timeout=30;";
+
+            const string storedProcedure = "AddData";
+
+            using (var conn = new SqlConnection(cnn))
+            {
+                conn.Open();
+
+                using (var dbCmd = new SqlCommand(storedProcedure, conn) { CommandType = CommandType.StoredProcedure })
+                {
+                    foreach (PropertyDescriptor prop in TypeDescriptor.GetProperties(myData))
+                    {
+                        DataAttribute attr = (DataAttribute)prop.Attributes[typeof(DataAttribute)];
+
+                        // if there's no attribute on the property, i.e. a private field it will be ignored.
+                        if (attr == null)
+                        {
+                            continue;
+                        }
+
+                        // don't add the properties value when attr.Include is false.
+                        if (!attr.Include)
+                        {
+                            continue;
+                        }
+
+                        dbCmd.Parameters.AddWithValue(attr.FieldName, prop.GetValue(myData));
+                    }
+                    // The result is the number of rows affected, it should always be 1
+                    var valke = dbCmd.ExecuteNonQuery();
+
+                    Console.WriteLine(valke.ToString());
+                    //return dbCmd.ExecuteNonQuery() == 1;
+
+                    return valke == -1;
+                }
+
+            }
+        }
+    }
+}
